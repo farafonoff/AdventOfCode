46260e04254b88499b9672dd74ec482ce094b9d5
farafonoff <farafonoff@gmail.com>
2017-12-01 09:20:37 +0400

    day1: 362&380 positions :(

--- 2017/01/01.js
+++ 2017/01/01.js
@@ -5,4 +5,29 @@ const PQ = require('js-priority-queue')
 
 var contents = fs.readFileSync('input', 'utf8').split("\n");
 
-console.log('Hello world!');
+let puz = 2;
+
+function cnext(list, pos) {
+    let op = pos;
+    if (puz === 1) {
+        op += 1;
+    }
+    if (puz === 2) {
+        op+=list.length/2;
+    }
+    op = op%list.length;
+    return op;
+}
+
+console.log(contents[0]);
+contents.forEach(is => {
+    let as = is.trim().split('');
+    let a = as.reduce((pv, cv,i,list) => {
+        let next = list[cnext(list, i)];
+        if (next === cv) {
+            pv.s+=Number(cv);
+        }
+        return pv;
+    }, {s:0,ch:''});
+    console.log(a);
+})
