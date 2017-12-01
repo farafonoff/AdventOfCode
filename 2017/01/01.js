163972caa705367259a8ac504bfc585990204f90
farafonoff <farafonoff@gmail.com>
2017-12-01 08:41:45 +0400

    initial template

---
+++ 2017/01/01.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const HM = require('hashmap')
+const md5 = require('js-md5')
+const PQ = require('js-priority-queue')
+
+var contents = fs.readFileSync('input', 'utf8').split("\n");
+
+console.log('Hello world!');
