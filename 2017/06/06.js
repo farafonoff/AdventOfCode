a1ace60c74de6b722a7cd752e8d089e23241b64a
farafonoff <farafonoff@gmail.com>
2017-12-06 09:17:02 +0400

    day 6: 311 & 218, added lodash for further days

--- 2017/06/06.js
+++ 2017/06/06.js
@@ -6,4 +6,28 @@ const PQ = require('js-priority-queue')
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
 var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 
-console.log(contents);
+function distribute(row) {
+    let maxVal = Math.max.apply(Math, row);
+    let maxIdx = row.findIndex(v => v === maxVal);
+    row[maxIdx] = 0;
+    for(let i=0;i<maxVal;++i) {
+        let tg = (1+maxIdx+i)%row.length;
+        ++row[tg];
+    }
+}
+
+contents.forEach(puz => {
+    console.log(puz);
+    let hm = new HM();
+    let op = 0;
+    let pd = puz.join('|');
+    do {
+        hm.set(pd, op);
+        ++op;
+        distribute(puz);
+        pd = puz.join('|');
+    } while (!hm.has(pd));
+    console.log(op);
+    console.log(op - hm.get(pd));
+})
+
