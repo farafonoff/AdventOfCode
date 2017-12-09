5292f9f9db874d8ec3b1417f6ef2ac091ced1a31
farafonoff <farafonoff@gmail.com>
2017-12-09 09:36:03 +0400

    day 9: 689 & 661

--- 2017/09/09.js
+++ 2017/09/09.js
@@ -5,8 +5,50 @@ const PQ = require('js-priority-queue')
 const _ = require('lodash')
 
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0)/*.map(s => s.split(/[ \t]/).map(Number))*/;
 
+function count(line, score, start) {
+    let garbage = false;
+    let cancel = false;
+    let idx = start;
+    let currentScore = score;
+    let balance = 1;
+    let garbages = 0;
+    while (balance > 0 && idx < line.length) {
+        //console.log(`${idx} ${line[idx]} ${cancel} ${garbage}`);
+        if (!cancel) {
+            if (garbage && line[idx] === '!') {
+                cancel = true;
+            }
+            if (line[idx] === '>') {
+                garbage = false;
+            }
+            if (garbage && !cancel) {
+                garbages += 1;
+            }
+            if (line[idx] === '<') {
+                garbage = true;
+            }
+        } else {
+            cancel = false;
+        }
+        if (!garbage) {
+            if (line[idx] === '}') {
+                balance -= 1;
+                score -= 1;
+            }
+            if (line[idx] === '{') {
+                score += 1;
+                currentScore += score;
+                balance += 1;
+            }
+        }
+        ++idx;
+    }
+    return [currentScore, garbages];
+}
+console.log('part1, part2')
 contents.forEach(line => {
-    console.log(line);
+    //console.log(line);
+    console.log(count(line.split(''), 1, 1));
 })
---
+++ 2017/10/10.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const HM = require('hashmap')
+const md5 = require('js-md5')
+const PQ = require('js-priority-queue')
+const _ = require('lodash')
+
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+
+contents.forEach(line => {
+    console.log(line);
+})
