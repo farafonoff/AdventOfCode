1137bd156573add7e406aecf8e75c8a2c2192941
farafonoff <farafonoff@gmail.com>
2017-12-16 09:56:08 +0400

    day 16: 320&281

--- 2017/16/16.js
+++ 2017/16/16.js
@@ -15,8 +15,38 @@ function decimalToHex(d, padding) {
 }
 
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[,]/));
 
+let letterz = "abcdefghijklmnop";
 contents.forEach(line => {
-    console.log(line);
+    let len = 16;
+    let arr = letterz.substring(0,len).split('');
+    let reps = 1000000000;
+    for (let lp = 0;lp<reps;++lp) {
+    line.forEach(cmd => {
+        let mo;
+        if (mo=cmd.match(/^s(\d+)$/)) {
+            let d = Number(mo[1]);
+            arr = arr.slice(-d).concat(arr.slice(0,-d));
+        } else
+        if (mo=cmd.match(/^x(\d+)\/(\d+)$/)) {
+            let d = Number(mo[1]);
+            let d1 = Number(mo[2]);
+            [arr[d],arr[d1]] = [arr[d1],arr[d]];
+        } else
+        if (mo=cmd.match(/^p([a-p])\/([a-p])$/)) {
+            let d = mo[1];
+            let d1 = mo[2];
+            arr = arr.map(c => c===d1?d:(c===d?d1:c))
+        }
+    })
+    if (lp===0) {
+        console.log(`Part 1 ${arr.join('')}`);
+    }
+    if (arr.join('')===letterz) {
+        reps = reps%(lp+1)+lp+1;
+        console.log('period ', lp+1)
+    }
+    }
+    console.log(`Part 2 ${arr.join('')}`);
 })
