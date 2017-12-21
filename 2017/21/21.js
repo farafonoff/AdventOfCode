5941cc2fd2ee50a407584ae0dbe535eda57d087b
farafonoff <farafonoff@gmail.com>
2017-12-21 09:51:11 +0400

    day 21

--- 2017/21/21.js
+++ 2017/21/21.js
@@ -14,9 +14,86 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+function r(arr, flip) {
+    let narr = [];
+    let len = arr.length;
+    //console.log(arr);
+    for(let x=0;x<len;++x) {
+        narr[x] = [];
+        for(let y=0;y<len;++y) {
+            if (flip) {
+                narr[x][y] = arr[x][len-y-1];
+            } else {
+                narr[x][y] = arr[len-y-1][x];
+            }
+        }
+    }
+    return narr;
+}
 
-contents.forEach(line => {
-    console.log(line);
-})
+function apply(arr, patterns, len) {
+    let result = [];
+    for(let i=0;i<arr.length;i+=len) {
+        for(let j=0;j<arr.length;j+=len) {
+            let tarr = []
+            let options = [];
+            for(let x=0;x<len;++x) {
+                tarr[x] = [];
+                for(let y=0;y<len;++y) {
+                    tarr[x][y] = arr[i+x][j+y];
+                }
+            }
+            for(let i=0;i<4;++i) {
+                options[i] = tarr.map(r=>r.join('')).join('/');
+                tarr = r(tarr,false);
+            }
+            tarr = r(tarr, true);
+            for(let i=4;i<8;++i) {
+                tarr = r(tarr,false);
+                options[i] = tarr.map(r=>r.join('')).join('/');
+            }
+            //console.log(options);
+            let found = patterns.filter(pattern => options.indexOf(pattern[0])!=-1)[0];
+            //console.log(patterns.filter(pattern => options.indexOf(pattern[0])!=-1).length);
+            if (!found) return arr;
+            //console.log(found);
+            let rarr = found[1].split('/').map(r => r.split(''));
+            let sx = (i/len)*(len+1);
+            let sy = (j/len)*(len+1);
+            for(let x=0;x<len+1;++x) {
+                result[sx+x] = result[sx+x]||[];
+                for(let y=0;y<len+1;++y) {
+                    result[sx+x][sy+y] = rarr[x][y];
+                }
+            }            
+            //console.log(found);
+        }
+    }
+    return result;
+}
+
+let seed = ['.#.','..#','###'].map(r=>r.split(''));
+
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/ => /));
+let current = seed;
+for(let iter=0;iter<18;++iter) {
+    let len = 0;
+    if (current.length%2==0) {
+        len = 2;        
+    } else 
+    if (current.length%3==0) {
+        len = 3;
+    }
+    let patterns = contents.filter(c=>c[0].length == len*len+len-1);
+    console.log(current);
+    //console.log(patterns);
+    current = apply(current, patterns, len);
+}
+let a1 = 0;
+for(let i=0;i<current.length;++i) {
+    for(j=0;j<current.length;++j) {
+        if (current[i][j]==='#') ++a1;
+    }
+}
+console.log(a1);
