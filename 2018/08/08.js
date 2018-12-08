41ff433fd751e082a6217a25bb9232cf874af798
farafonoff <farafonoff@gmail.com>
2018-12-08 09:29:49 +0400

    day 8

--- 2018/08/08.js
+++ 2018/08/08.js
@@ -1,3 +1,4 @@
+// 8   00:18:26   574      0   00:27:47   513      0
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -14,9 +15,62 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/ /).map(Number));
+
+function sum(ar) {
+    return ar.reduce((s,c)=>s+c,0)
+}
+
+var r1 = [];
+
+function s1(input) {
+    let [ch,md,...rest] = input;
+    //console.log(ch, md, rest);
+    if (ch === 0) {
+        r1 = [...r1, ...rest.slice(0, md)];
+        //console.log(rest.slice(0, md))
+        return 2+md;
+    } else {
+        var chls = 0;
+        for(let i=0;i<ch;++i) {
+            var chl = s1(rest.slice(chls))
+            chls += chl;
+            //console.log(chl, chls, rest.slice(chls));
+        }
+        //console.log(chls, rest.slice(chls, chls+md))
+        r1 = [...r1, ...rest.slice(chls, chls+md)];
+        return 2+md + chls;
+    }
+}
+
+function s2(input) {
+    let [ch,md,...rest] = input;
+    //console.log(ch, md, rest);
+    if (ch === 0) {
+        r1 = [...r1, ...rest.slice(0, md)];
+        return [2+md, sum(rest.slice(0, md))];
+    } else {
+        var chls = 0;
+        let values = [];
+        for(let i=0;i<ch;++i) {
+            var [chl, value] = s2(rest.slice(chls))
+            values.push(value);
+            chls += chl;
+            //console.log(chl, chls, rest.slice(chls));
+        }
+        var mdd = rest.slice(chls, chls+md);
+        var nv = 0;
+        mdd.forEach(mde => nv+=(values[mde-1]||0));
+        //console.log(chls, rest.slice(chls, chls+md))
+        r1 = [...r1, ...mdd];
+        return [2+md + chls, nv];
+    }
+}
 
 contents.forEach(line => {
-    console.log(line);
+    //s1(line);
+    var r2 = s2(line)
+    console.log(sum(r1));
+    console.log(r2);
 })
