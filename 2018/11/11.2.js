87a43d3689e21918ec69ff97fd00fdccfa8b6f1a
farafonoff <farafonoff@gmail.com>
2018-12-11 09:50:32 +0400

    day 11

--- 2018/11/11.2.js
+++ 2018/11/11.2.js
@@ -1,3 +1,4 @@
+//11   00:13:06   447      0   00:46:34   841      0
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -38,24 +39,56 @@ function power(s,x,y) {
     hd -= 5;
     return hd;
 }
-console.log(power(57,122,79))
-console.log(power(39,217,196))
-console.log(power(71,101,153))  
+
 let maxx = 0;
 let ress = "";
+var sgrid = [];
+var put = (x,y,v) => sgrid[x*300+y] = v;
+var get = (x,y) => sgrid[x*300+y];
+function comp(s,x,y) {
+    let ch = get(x,y);
+    if (ch!==undefined) return ch;
+    if (!isFinite(power(s,x,y))) {
+        put(x,y,0);
+        return 0;
+    }
+    else {
+        let c = power(s,x,y);
+        let r1 = comp(s,x-1,y);
+        let r2 = comp(s,x,y-1);
+        let r3 = comp(s,x-1,y-1);
+        let sum = r1+r2-r3+c;
+        put(x,y,sum);
+        return sum;
+    }
+}
 for(let x=1;x<=300;++x) {
     for(let y=1;y<=300;++y) {
-        let tp = 0;
-        for(let t = 0;t<3;++t) {
-            for(let v = 0;v<3;++v) {
-                tp+=power(input,x+t, y+v);
-            }
-        }
-        if (tp > maxx) {
-            maxx = tp;
-            ress = `${x},${y}`
-        }
+        comp(input,x,y);
     }
 }
 
-console.log(maxx,ress);
+function s2(x,y,si) {
+    let x1 = x+si;
+    let y1 = y+si;
+    let g1 = get(x1, y);
+    let g2 = get(x, y1);
+    let g3 = get(x, y);
+    let g4 = get(x1, y1);
+    let sum = g4-g1-g2+g3;
+    return sum;
+}
+
+for(let x=1;x<=300;++x) {
+    for(let y=1;y<=300;++y) {
+        for(let si = 1;si<=300;++si) {
+            if (x+si>300||y+si>300) break;
+            let sum = s2(x-1,y-1,si);
+            if (sum > maxx) {
+                maxx = sum;
+                ress = `${x},${y},${si}`
+            }
+        }
+    }
+}
+console.log(input,maxx,ress);
