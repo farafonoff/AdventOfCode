5818bec1d7c345f653723cd56dea328ccf1acfb2
farafonoff <farafonoff@gmail.com>
2018-12-14 09:21:03 +0400

    day 14

--- 2018/14/14.js
+++ 2018/14/14.js
@@ -1,3 +1,4 @@
+//  14   00:11:28   183      0   00:16:50    72     29
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -14,9 +15,42 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+var input = 290431;
+var input2 = ''+input;
+var offset = (3+input2.length);
+var s2found = false;
+var s2;
 
-contents.forEach(line => {
-    console.log(line);
-})
+var recipes = [3,7]
+var elves = [0,1]
+
+function step() {
+    let sum = elves.reduce((s,e) => s+recipes[e], 0)
+    let sstr = ''+sum;
+    let digits = sstr.split('').map(Number);
+    digits.forEach(digit => recipes.push(digit));
+    if (!s2found) {
+        let tail = recipes.slice(recipes.length-offset, recipes.length).join('')
+        let io2 = tail.indexOf(input2);
+        if (io2!==-1) {
+            s2found = true;
+            s2 = recipes.length-offset+io2;
+            console.log(s2);
+        }    
+    }
+    elves = elves.map(elve => {
+        let pos = elve + 1 + recipes[elve];
+        pos = pos%recipes.length;
+        return pos;
+    })
+}
+
+while(recipes.length<input+10) {
+    step();
+    //console.log(elves, recipes);
+}
+console.log(recipes.slice(input, input+10).join(''))
+while(!s2found) {
+    step();
+    //console.log(elves, recipes);
+}
