ef28309f7c84405a515c17849ffcbe58cca10c63
Artem Farafonov <artem_farafonov@epam.com>
2018-12-17 13:06:12 +0400

    day 17

--- 2018/17/17.js
+++ 2018/17/17.js
@@ -1,3 +1,4 @@
+// 17   03:44:37   617      0   04:03:58   631      0
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -18,7 +19,7 @@ var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).f
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 var re = /([xy])=(\d+), ([xy])=(\d+)..(\d+)/
 let xr = [Infinity, -Infinity]
-let yr = [0, -Infinity]
+let yr = [Infinity, -Infinity]
 let hm = new HM();
 contents.forEach(line => {
     var match = re.exec(line);
@@ -26,10 +27,10 @@ contents.forEach(line => {
         let v1 = match[1];
         let v2 = match[3];
         let v1v = Number(match[2])
-        let v2r = [Number(match[4]),Number(match[5])]
+        let v2r = [Number(match[4]), Number(match[5])]
         if (v1 === 'x') {
-            for(let y = v2r[0];y<=v2r[1];++y) {
-                hm.set([v1v,y],{t:'#'});
+            for (let y = v2r[0]; y <= v2r[1]; ++y) {
+                hm.set([v1v, y], { t: '#' });
             }
             yr[0] = Math.min(yr[0], v2r[0])
             yr[1] = Math.max(yr[1], v2r[1])
@@ -37,8 +38,8 @@ contents.forEach(line => {
             xr[1] = Math.max(xr[1], v1v)
         }
         if (v1 === 'y') {
-            for(let x = v2r[0];x<=v2r[1];++x) {
-                hm.set([x,v1v],{t:'#'});
+            for (let x = v2r[0]; x <= v2r[1]; ++x) {
+                hm.set([x, v1v], { t: '#' });
             }
             xr[0] = Math.min(xr[0], v2r[0])
             xr[1] = Math.max(xr[1], v2r[1])
@@ -47,70 +48,81 @@ contents.forEach(line => {
         }
     }
 })
-console.log(xr,yr);
+//console.log(xr, yr);
 dump = (log) => {
     let res = 0;
-    for(let y = yr[0]; y<=yr[1];++y){
+    let r2 = 0;
+    for (let y = yr[0]; y <= yr[1]; ++y) {
         let ln = '';
-        for(let x = xr[0]; x<=xr[1];++x){
-            if (hm.has([x,y])) {
-                if (hm.get([x,y]).t!='#') ++res;
-                ln+=hm.get([x,y]).t
+        for (let x = xr[0]-2; x <= xr[1]+2; ++x) {
+            if (hm.has([x, y])) {
+                if (hm.get([x, y]).t != '#')++res;
+                if (hm.get([x, y]).t === '~')++r2;
+                ln += hm.get([x, y]).t
             } else {
                 ln += '.';
             }
         }
         if (log) console.log(ln);
     }
-    return res;
+    return [res,r2];
 }
 //dump(true);
-let fd = (stream) => {let r = [...stream]; r[1]+=1; return r;}
-let fu = (stream) => {let r = [...stream]; r[1]-=1; return r;}
-let fr = (stream) => {let r = [...stream]; r[0]+=1; return r;}
-let fl = (stream) => {let r = [...stream]; r[0]-=1; return r;}
-let streams = [[500,0]];
-for(let round=0;round<2000;++round) {
-    streams.forEach((stream,idx) => {
-        if (hm.has(fd(stream))){
-            if (true || hm.get(fd(stream)).t === '#') {
-                let lp = fl(stream);
-                let rp = fr(stream);
-                hm.set(stream, {t:'~'})
-                while(!hm.has(lp)&&hm.has(fd(lp))) {
-                    hm.set(lp, {t:'~'})
+let fd = (stream) => { let r = [...stream]; r[1] += 1; return r; }
+let fu = (stream) => { let r = [...stream]; r[1] -= 1; return r; }
+let fr = (stream) => { let r = [...stream]; r[0] += 1; return r; }
+let fl = (stream) => { let r = [...stream]; r[0] -= 1; return r; }
+let streams = [[500, 0]];
+//hm.set(streams[0], {t:'+'})
+for (let round = 0; round < 10000; ++round) {
+    streams.forEach((stream, idx) => {
+        let bo = _.get(hm.get(fd(stream)), 't', '.')
+        if (bo === '|') {
+            streams[idx] = undefined;            
+        } else
+        if (/*hm.has(fd(stream))*/ bo!=='.') {
+            let bottom = stream;
+            {
+                let lp = fl(bottom);
+                let rp = fr(bottom);
+                hm.set(bottom, { t: '~' })
+                while ((_.get(hm.get(lp), 't') !== '#') && (['#','~'].includes(_.get(hm.get(fd(lp)), 't')))) {
+                    hm.set(lp, { t: '~' })
                     lp = fl(lp)
                 }
-                while(!hm.has(rp)&&hm.has(fd(rp))) {
-                    hm.set(rp, {t:'~'})
+                while ((_.get(hm.get(rp), 't') !== '#') && (['#','~'].includes(_.get(hm.get(fd(rp)), 't')))) {
+                    hm.set(rp, { t: '~' })
                     rp = fr(rp)
                 }
+                let dl = _.get(hm.get(fd(lp)), 't');
+                let dr = _.get(hm.get(fd(rp)), 't');
                 let overflow = false;
-                if (!hm.has(fd(rp))) {
+                if (!(['#','~'].includes(_.get(hm.get(fd(rp)), 't')))) {
                     overflow = true;
-                    hm.set(rp, {t:'|'})
+                    hm.set(rp, { t: '|' })
                     streams.push(rp)
                 }
-                if (!hm.has(fd(lp))) {
+                if (!(['#','~'].includes(_.get(hm.get(fd(lp)), 't')))) {
                     overflow = true;
-                    hm.set(lp, {t:'|'})
+                    hm.set(lp, { t: '|' })
                     streams.push(lp)
                 }
                 if (overflow) {
                     streams[idx] = undefined;
+                    for(let i=lp[0]+1;i<rp[0];++i) {
+                        hm.set([i, lp[1]], { t: '|' })
+                    }
                 } else {
                     streams[idx] = fu(stream);
-                }    
-            } else {
-                streams[idx] = undefined;
+                }
             }
         } else {
             streams[idx] = fd(stream);
-            hm.set(fd(stream), {t:'|'})
+            hm.set(fd(stream), { t: '|' })
         }
     })
-    streams = streams.filter(stream => !!stream)
-    if (round%1000==0) console.log(round, streams);
+    streams = _.uniqBy(streams.filter(stream => !!stream), v=>JSON.stringify(v))
+    //if (round % 100 == 0) console.log(round, streams);
     /*let water = hm.keys().reduce((s,key) => {
         if (hm.get(key).t!=='#') {
             return s+1;
@@ -126,5 +138,5 @@ for(let round=0;round<2000;++round) {
     }*/
 }
 let water = dump(true);
-console.log(streams);
+//console.log(streams);
 console.log(water)
