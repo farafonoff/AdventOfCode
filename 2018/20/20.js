1e9b8b5fdf401fd58b2892c004271b4a253116ad
farafonoff <farafonoff@gmail.com>
2018-12-20 10:19:54 +0400

    20 wip

--- 2018/20/20.js
+++ 2018/20/20.js
@@ -16,7 +16,47 @@ function decimalToHex(d, padding) {
 
 var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+let directions = {
+    S: [0, 1],
+    N: [0, -1],
+    E: [1, 0],
+    W: [-1, 0],
+};
+
+function walk(arr, idx, paths) {
+    let result = '';
+    for(let i=idx;i<arr.length;++i) {
+        console.log(arr[i])
+        if (arr[i] === '(') {
+            let branches = [];
+            while(arr[i]!==')') {
+                let [res, nidx] = walk(arr, i+1, paths);
+                console.log(res)
+                i = nidx;
+                branches.push(res);
+            }
+            console.log(branches);
+            result += Math.min.apply(null, branches);
+        } else
+        if (arr[i] === '|') {
+            return [result, i]
+        } else
+        if (arr[i] === ')') {
+            return [result, i]
+        } else {
+            result += arr[i];
+        }
+    }
+    return [result];
+}
+
+function solve(str) {
+    let arr = `(${str.slice(1, str.length-1)})`.split('');
+    return walk(arr, 0)
+}
+
+
 
 contents.forEach(line => {
-    console.log(line);
+    console.log(solve(line));
 })
