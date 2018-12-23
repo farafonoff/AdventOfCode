fa815a32e690cc5bddb26875ced7731d9a9592f2
farafonoff <farafonoff@gmail.com>
2018-12-23 09:26:59 +0400

    day23 wip

--- 2018/23/23.js
+++ 2018/23/23.js
@@ -16,7 +16,22 @@ function decimalToHex(d, padding) {
 
 var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
-
+var rex = /pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(-?\d+)/
+let bots = []
 contents.forEach(line => {
-    console.log(line);
+    if (rex.exec(line)) {
+        bots.push(rex.exec(line).slice(1).map(Number))
+    }
 })
+let max = bots.reduce((max, current) => current[3]>max[3]?current:max,bots[0]);
+let inrange = bots.filter(bot => {
+    let dist = bot.slice(0,3).map((c,idx)=>Math.abs(max[idx]-c)).reduce((s,v)=>s+v,0)
+    return dist<=max[3];
+})
+console.log(max, inrange.length)
+
+let xss = bots.map((bot,idx) => [bot[0]-bot[3],bot[0]+bot[3],idx]);
+let yss = bots.map((bot,idx) => [bot[1]-bot[3],bot[1]+bot[3],idx]);
+let zss = bots.map((bot,idx) => [bot[2]-bot[3],bot[2]+bot[3],idx]);
+xss.sort((b1,b2) => b1[0]-b2[0]);
+console.log(xss)
