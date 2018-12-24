0b7a6ad232ffbe08905d09e00f3d914220b249cc
farafonoff <farafonoff@gmail.com>
2018-12-24 10:05:38 +0400

    24 wip

--- 2018/24/24.js
+++ 2018/24/24.js
@@ -37,6 +37,7 @@ contents.forEach(line => {
         groupid = 0;
     }
     if (regexOuter.exec(line)) {
+        ++groupid
         let data = regexOuter.exec(line)
         let group = { army: state, armyss, groupid, units: Number(data[1]), hp: Number(data[2]), dmg: Number(data[4]), initiative: Number(data[6]), att: data[5], def: [], weak: []}
         let deftypes = data[3];
@@ -54,9 +55,13 @@ contents.forEach(line => {
 })
 let fighters = [].concat(immune, infect)
 
+function effectivePower(group1) {
+    return group1.units*group1.dmg;
+}
+
 function damage(group1, group2) {
     if (group2.def.indexOf(group1.att)!==-1) return 0;
-    let total = group1.units*group1.dmg;
+    let total = effectivePower(group1);
     if (group2.weak.indexOf(group1.att)!==-1) return total*2;
     else return total;
 }
@@ -64,41 +69,59 @@ function damage(group1, group2) {
 function damage2(group1, group2) {
     let base = damage(group1, group2)
     let kills = Math.floor(base/group2.hp);
+    if (kills > group2.units) kills = group2.units;
+    //console.log(group1, group2, base, kills)
     group2.units -= kills;
-    if (group2.units<0) group2.units = 0;
+}
+
+function cmpSelection(group1, group2) {
+    if (effectivePower(group1) != effectivePower(group2))
+        return effectivePower(group2) - effectivePower(group1);
+    else
+        return group2.initiative - group1.initiative;
 }
 
 function turn() {
     //selection
-    let selections = fighters.map(fg => {
+    let selections = []
+    fighters.sort(cmpSelection)
+    fighters.forEach(fg => {
         let maxDamage = -1;
         let targetGroup = 0;
         fighters.forEach((trg, idx) => {
             if (trg.army === fg.army) return;
+            if (selections.includes(trg)) return;
             let dmg = damage(fg, trg)
             if (dmg>maxDamage) {
                 maxDamage = dmg
                 targetGroup = idx;
             }
         })
-        console.log(fg, `would deal ${maxDamage} to`, fighters[targetGroup])
-        return targetGroup;
+        //console.log(fg, `would deal ${maxDamage} to`, fighters[targetGroup])
+        return selections.push(targetGroup);
     })
+    //console.log(selections)
     //attack
     let order = fighters.map((f, idx) => [idx, f.initiative])
-    order.sort((o1,o2) => o1[1]-o2[1])
+    order.sort((o1,o2) => o2[1]-o1[1])
+    //console.log(order)
     order.forEach(op => {
         let f1 = fighters[op[0]]
         let f2 = fighters[selections[op[0]]]
-        if (f1.units>0) {
-            damage2(f1, f2);
+        if (f1.army!=f2.army) {
+            if (f1.units>0) {
+                damage2(f1, f2);
+            }
         }
     })
     //remove
     fighters = fighters.filter(fi => fi.units>0)
 }
-
-for(let i=0;i<200;++i) {
-    turn();
+let end = false;
+for(let i=0;i<140;++i) {
+    end = turn();
 }
+let sum = 0
+fighters.forEach(fi => sum+=fi.units)
 console.log(fighters)
+console.log(sum)
