e01af91131fb76421e370fd9ba6e071eebf73432
farafonoff <farafonoff@gmail.com>
2018-12-25 09:24:23 +0400

    day 25

--- 2018/25/25.js
+++ 2018/25/25.js
@@ -1,3 +1,4 @@
+// 25   00:22:10   281      0   00:22:31   225      0
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -14,9 +15,44 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
-
-contents.forEach(line => {
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[, ]/).map(Number));
+/*contents.forEach(line => {
     console.log(line);
+})*/
+let trh = 3;
+let mhd = (a1,a2) => {
+    return a1.reduce((s,e,idx) => s+Math.abs(e-a2[idx]), 0)
+}
+let graph = contents.map(v1 => {
+    return contents.map(v2 => {
+        //console.log(v1,v2,mhd(v))
+        if (mhd(v1,v2)<=trh) {
+            return 1;
+        } else {
+            return 0;
+        }
+    })
 })
+//console.log(graph)
+let components = contents.map(() => -1)
+function compy(sidx, mrk) {
+    components.forEach((cmp, idx) => {
+        if (sidx !== idx) {
+            if (cmp===-1 && graph[sidx][idx] === 1) {
+                components[idx] = mrk;
+                compy(idx,mrk)
+            }
+        }
+    })
+}
+
+let sidx;
+while((sidx = components.indexOf(-1))!==-1) {
+    components[sidx] = sidx
+    compy(sidx,sidx)
+    //console.log(components)
+}
+
+let distinct = _.uniq(components);
+console.log(distinct.length)
