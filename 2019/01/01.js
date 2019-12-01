95b5b9daf20ff2b2c731dec6ab9a0719cbbeeb8a
farafonoff <farafonoff@gmail.com>
2019-12-01 09:06:33 +0400

    day1

--- 2019/01/01.js
+++ 2019/01/01.js
@@ -1,3 +1,4 @@
+//   1   00:02:10   298      0   00:05:16   184      0
 const fs = require('fs');
 const HM = require('hashmap')
 const md5 = require('js-md5')
@@ -14,9 +15,19 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(Number);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
-
+let acc1 = 0;
+let acc2 = 0;
+let fmf = (m) => (Math.floor(m/3)-2)
 contents.forEach(line => {
-    console.log(line);
+    let fm = fmf(line);
+    acc1 += fm;
+    acc2 += fm;
+    while(fm>0) {
+        fm = fmf(fm);
+        if (fm>0) acc2+=fm;
+    }
+    //console.log(line);
 })
+console.log(acc1, acc2)
