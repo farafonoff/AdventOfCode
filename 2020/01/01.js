4949f4d2d016fe0cfa317cdb4ef3b4ef18ae7662
Artem Farafonov <artem_farafonov@epam.com>
2020-12-01 09:11:52 +0400

    day 1

--- 2020/01/01.js
+++ 2020/01/01.js
@@ -14,9 +14,30 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(Number);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 
-contents.forEach(line => {
-    console.log(line);
-})
+try {
+    contents.forEach(cn => {
+        contents.forEach(cn1 => {
+            if (cn + cn1 === 2020) {
+                console.log('Part 1', cn*cn1)
+                throw 0;
+            }
+        })
+    })
+}
+catch(e) {}
+try {
+    contents.forEach(cn => {
+        contents.forEach(cn1 => {
+            contents.forEach(cn2 => {
+                if (cn + cn1 + cn2 === 2020) {
+                    console.log('Part 2', cn*cn1*cn2)
+                    throw 0;
+                }
+            })
+        })
+    })    
+}
+catch(e) {}
