f9924f4ccbe05e9f6ffc47f12cefd18bd177a794
Artem Farafonov <artem_farafonov@epam.com>
2020-12-06 09:15:56 +0400

    day 6

--- 2020/06/06.js
+++ 2020/06/06.js
@@ -14,9 +14,55 @@ function decimalToHex(d, padding) {
     return hex;
 }
 
-var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0);
+var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim())
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+let cgroup = {}
+let allgroups = []
+contents.push("")
 contents.forEach(line => {
-    console.log(line);
+    if (line.length>0) {
+        const lar = line.split('')
+        lar.forEach(lch => {
+            cgroup[lch] = true
+        })
+    } else {
+        allgroups.push(cgroup)
+        cgroup = {}
+    }
 })
+let ans = 0;
+allgroups.forEach(grp => {
+    ans += Object.keys(grp).length
+})
+console.log(ans)
+
+allgroups = []
+cgroup = {}
+ccgroup = []
+ans = 0
+contents.forEach(line => {
+    if (line.length>0) {
+        const lar = line.split('')
+        ccgroup.push(lar)
+    } else {
+        for(let ch="a".charCodeAt(0);ch<="z".charCodeAt(0);++ch) {
+            let allyes = true;
+            ccgroup.forEach(grp => {
+                if (grp.indexOf(String.fromCharCode(ch))===-1) {
+                    allyes = false
+                }
+            })
+            if (allyes) {
+                cgroup[String.fromCharCode(ch)] = true
+            }
+        }
+        allgroups.push(cgroup)
+        cgroup = {}
+        ccgroup = []
+    }
+})
+allgroups.forEach(grp => {
+    ans += Object.keys(grp).length
+})
+console.log(ans)
