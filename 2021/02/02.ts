ebf3fae92739d347d1f61570c9dc651748bebf40
Artem Farafonov <artem_farafonov@epam.com>
2021-12-02 09:07:29 +0400

    day 2

--- 2021/02/02.ts
+++ 2021/02/02.ts
@@ -30,9 +30,45 @@ var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);
+  .filter((s) => s.length > 0)
+  .map((s) => s.split(" "))
+  .map((ve) => ({ cmd: ve[0], prm: Number(ve[1]) }));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+let depth = 0;
+let x = 0;
 contents.forEach((line) => {
-  console.log(line);
+  switch (line.cmd) {
+    case "forward":
+      x += line.prm;
+      break;
+    case "up":
+      depth -= line.prm;
+      break;
+    case "down":
+      depth += line.prm;
+      break;
+  }
 });
+
+console.log(x, depth, x * depth);
+
+depth = 0;
+x = 0;
+let aim = 0;
+contents.forEach((line) => {
+  switch (line.cmd) {
+    case "forward":
+      x += line.prm;
+      depth += line.prm * aim;
+      break;
+    case "up":
+      aim -= line.prm;
+      break;
+    case "down":
+      aim += line.prm;
+      break;
+  }
+});
+
+console.log(x, depth, x * depth);
