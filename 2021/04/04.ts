3c78cae6f15e804f58b2dd0df8e4c6047d8e5808
Artem Farafonov <artem_farafonov@epam.com>
2021-12-04 09:25:32 +0400

    day 4

--- 2021/04/04.ts
+++ 2021/04/04.ts
@@ -33,6 +33,52 @@ var contents = fs
   .filter((s) => s.length > 0);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
+let seq = contents[0].split(",").map(Number);
+let grids = contents.slice(1);
+let grdata = [];
+let markz = new HM();
+let winners = new HM();
+for (let i = 0; i < grids.length / 5; ++i) grdata.push([]);
+
+grids.forEach((line, idx) => {
+  grdata[Math.floor(idx / 5)][idx % 5] = line.split(/[ ]+/).map(Number);
 });
+let answers = [];
+
+//console.log(grdata);
+seq.forEach((inum) => {
+  grdata.forEach((grid, gridid) => {
+    if (winners.has(gridid)) return;
+    grid.forEach((row, rowid) => {
+      row.forEach((col, colid) => {
+        if (col === inum) {
+          markz.set([gridid, rowid, colid], true);
+          let marks1 = 0;
+          grid.forEach((wrow, wrowid) => {
+            if (markz.has([gridid, wrowid, colid])) ++marks1;
+          });
+          let marks2 = 0;
+          row.forEach((wcol, wcolid) => {
+            if (markz.has([gridid, rowid, wcolid])) ++marks2;
+          });
+          if (marks1 === 5 || marks2 === 5) {
+            let winner = gridid;
+            let called = inum;
+            winners.set(winner, true);
+            //console.log("WIN", winner, called);
+            let sum = 0;
+            grdata[winner].forEach((row, rowid) => {
+              row.forEach((col, colid) => {
+                if (!markz.has([winner, rowid, colid])) sum += col;
+              });
+            });
+            //console.log(sum * called);
+            answers.push(sum * called);
+          }
+        }
+      });
+    });
+  });
+});
+console.log(_.first(answers));
+console.log(_.last(answers));
