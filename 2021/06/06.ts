ad49fed091a612b54f0baa9a24969e44cd53a0a1
Artem Farafonov <artem_farafonov@epam.com>
2021-12-06 09:14:24 +0400

    day 6

--- 2021/06/06.ts
+++ 2021/06/06.ts
@@ -30,9 +30,25 @@ var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);
+  .filter((s) => s.length > 0)
+  .map((s) => s.split(",").map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
-});
+let fishs = contents[0];
+fishs.sort((a, b) => a - b);
+let fishnum = [];
+for (let fn = 0; fn < 9; ++fn) fishnum[fn] = 0;
+fishs.forEach((fv) => fishnum[fv]++);
+const checkpoints = [80, 256];
+for (let cd = 0; cd < 256; ++cd) {
+  let fn2 = fishnum.map((fc, fv) => {
+    return fishnum[fv + 1];
+  });
+  fn2[8] = fishnum[0];
+  fn2[6] += fishnum[0];
+  fishnum = fn2;
+  let sum = fishnum.reduce((pv, cv) => pv + cv, 0);
+  if (checkpoints.includes(cd + 1)) {
+    console.log(cd + 1, sum);
+  }
+}
