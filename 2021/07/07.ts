b982e48083c5136580b269d52e2d12745624fbb7
Artem Farafonov <artem_farafonov@epam.com>
2021-12-07 09:19:46 +0400

    day 7

--- 2021/07/07.ts
+++ 2021/07/07.ts
@@ -30,9 +30,43 @@ var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);
+  .filter((s) => s.length > 0)
+  .map((s) => s.split(",").map(Number));
+let crabs = contents[0];
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
-});
+
+crabs.sort((a, b) => a - b);
+
+let len = crabs.length;
+let med;
+if (len % 2 === 0) {
+  med = (crabs[len / 2 - 1] + crabs[len / 2]) / 2;
+} else {
+  med = crabs[(len - 1) / 2];
+}
+
+let fuelz = crabs.map((c) => Math.abs(c - med)).reduce((pv, cv) => pv + cv, 0);
+console.log(fuelz);
+
+let sums = [0];
+for (let ss = 1; ss < 100000; ++ss) {
+  sums.push(_.last(sums) + ss);
+}
+//console.log(sums);
+
+let min = crabs[0];
+let max = _.last(crabs);
+let minff = Infinity;
+let rpos = 0;
+for (let pos = min; pos < max; ++pos) {
+  let fuelz = crabs
+    .map((c) => Math.abs(c - pos))
+    .map((pd) => sums[pd])
+    .reduce((pv, cv) => pv + cv, 0);
+  if (fuelz < minff) {
+    minff = fuelz;
+    rpos = pos;
+  }
+}
+console.log(minff);
