0ccb6fcd44af837c1e10e179323602637252706c
Artem Farafonov <artem_farafonov@epam.com>
2021-12-10 15:09:47 +0400

    day 10

--- 2021/10/10.ts
+++ 2021/10/10.ts
@@ -33,6 +33,50 @@ var contents = fs
   .filter((s) => s.length > 0);
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
+let ans1 = 0;
+let balls = {
+  ")": 3,
+  "]": 57,
+  "}": 1197,
+  ">": 25137,
+};
+let lines2 = contents
+  .map((line) => {
+    while (true) {
+      let oline = line
+        .replace("()", "")
+        .replace("[]", "")
+        .replace("{}", "")
+        .replace("<>", "");
+      if (oline === line) {
+        let illeg = line.split("").find((ch) => {
+          return [")", "]", "}", ">"].includes(ch);
+        });
+        if (illeg) {
+          ans1 += balls[illeg];
+          return false;
+        }
+        return oline;
+      }
+      line = oline;
+    }
+  })
+  .filter((line) => line !== false);
+
+console.log(ans1);
+let balls2 = {
+  "(": 1,
+  "[": 2,
+  "{": 3,
+  "<": 4,
+};
+let ballz = lines2.map((line) => {
+  let reversed = (line as string).split("").reverse();
+  let val = 0;
+  reversed.forEach((ch) => {
+    val = val * 5 + balls2[ch];
+  });
+  return val;
 });
+ballz.sort((a, b) => a - b);
+console.log(ballz[(ballz.length - 1) / 2]);
