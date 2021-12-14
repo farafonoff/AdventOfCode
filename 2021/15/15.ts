0dab282eb8b75ec7c08a8bec66b27310409815f1
Artem Farafonov <artem_farafonov@epam.com>
2021-12-14 09:34:33 +0400

    init day 15

---
+++ 2021/15/15.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import HM from "hashmap";
+import md5 from "js-md5";
+import PQ from "js-priority-queue";
+import _ from "lodash";
+const infile = process.argv[2] || "input";
+
+function trnum(val: string): number | string {
+  let nn = Number(val);
+  if (isFinite(nn)) {
+    return nn;
+  }
+  return val;
+}
+function decimalToHex(d, padding) {
+  var hex = Number(d).toString(16);
+  padding =
+    typeof padding === "undefined" || padding === null
+      ? (padding = 2)
+      : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
+}
+
+var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  .map((s) => s.trim())
+  .filter((s) => s.length > 0);
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+contents.forEach((line) => {
+  console.log(line);
+});
