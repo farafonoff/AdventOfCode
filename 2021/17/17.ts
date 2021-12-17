b5453a54d5fe6c86c30f01c29e2d1e9c4115501a
Artem Farafonov <artem_farafonov@epam.com>
2021-12-17 09:22:53 +0400

    day 17

--- 2021/17/17.ts
+++ 2021/17/17.ts
@@ -48,13 +48,55 @@ function answer(part, value) {
   console.log(`Answer ${part}: ${value}`);
 }
 
+/*var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  .map((s) => s.trim())
+  .filter((s) => s.length > 0);*/
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
-//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
-});
+  .filter((s) => s.length > 0)
+  .map((s) => s.match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/)); // [orig, g1, g2 ...] = content
+let [x1, x2, y1, y2] = contents[0].slice(1).map(Number);
+DEBUG = false;
+dbg([x1, x2, y1, y2]);
+function sim(xv, yv) {
+  let cc = [0, 0];
+  let top = 0;
+  while (cc[0] <= x2 + 1 && cc[1] >= y1 - 1) {
+    cc[0] += xv;
+    cc[1] += yv;
+    if (xv < 0) {
+      xv += 1;
+    }
+    if (xv > 0) {
+      xv -= 1;
+    }
+    yv -= 1;
+    if (cc[1] > top) top = cc[1];
+    if (cc[0] >= x1 && cc[0] <= x2 && cc[1] >= y1 && cc[1] <= y2) {
+      return top;
+    }
+  }
+  return false;
+}
+let topmost = -Infinity;
+let ans2 = 0;
+
+for (let xv = 0; xv <= x2 + 1; ++xv) {
+  for (let yv = -500; yv < 500; ++yv) {
+    let av = sim(xv, yv);
+    if (av !== false) {
+      dbg([xv, yv]);
+      ++ans2;
+      if (av > topmost) {
+        topmost = av;
+      }
+    }
+  }
+}
+answer(1, topmost);
+answer(2, ans2);
