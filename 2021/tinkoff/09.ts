1a7a00f06675ec8952309f5c0c475a1e79a5cb69
Artem Farafonov <artem_farafonov@epam.com>
2021-12-19 17:58:56 +0400

    tinkoff_9

---
+++ 2021/tinkoff/09.ts
@@ -0,0 +1,74 @@
+import * as fs from "fs";
+import HM from "hashmap";
+import md5 from "js-md5";
+import PQ from "js-priority-queue";
+import _ from "lodash";
+const infile = process.argv[2] || "input";
+
+function trnum(val: string): number | string {
+  let nn = Number(val);
+  if (isFinite(nn)) {
+    return nn;
+  }
+  return val;
+}
+function decimalToHex(d, padding) {
+  var hex = Number(d).toString(16);
+  padding =
+    typeof padding === "undefined" || padding === null
+      ? (padding = 2)
+      : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
+}
+
+function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
+  let ov = tab.get(key) || dv;
+  tab.set(key, ov + inc);
+}
+let DEBUG = true;
+
+function dbg(expression: any, message: string = ""): any {
+  if (!DEBUG) {
+    return expression;
+  }
+  if (message) {
+    console.log(message, expression);
+  } else {
+    console.log(expression);
+  }
+  return expression;
+}
+
+function answer(part, value) {
+  console.log(`Answer ${part}: ${value}`);
+}
+
+var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  .map((s) => s.trim())
+  .filter((s) => s.length > 0)
+  .map((s) => s.split(" ").map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+let data = contents[1];
+let buf = [];
+function findChain(sp, chl) {
+  if (buf[sp] !== undefined) {
+    return chl;
+  }
+  buf[sp] = chl;
+  let next = data[sp] - 1;
+  return findChain(next, chl + 1);
+}
+let chains = data.map((dv, idx) => {
+  buf = [];
+  return findChain(idx, 0);
+});
+dbg(chains);
+answer(1, Math.max.apply(null, chains));
