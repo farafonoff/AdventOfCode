f76a3728e50bffd6e12ac5dac861d53939feaeb8
Artem Farafonov <artem_farafonov@epam.com>
2021-12-24 13:46:10 +0400

    init day 25

---
+++ 2021/25/25.ts
@@ -0,0 +1,60 @@
+import * as fs from "fs";
+import HM from "hashmap";
+import md5 from "js-md5";
+import PQ from "js-priority-queue";
+import _ from "lodash";
+const infile = process.argv[2] || "input";
+
+function trnum(val: string): number | string {
+  let nn = Number(val);
+  if (isFinite(nn)) {
+    return nn;
+  }
+  return val;
+}
+function decimalToHex(d, padding) {
+  var hex = Number(d).toString(16);
+  padding =
+    typeof padding === "undefined" || padding === null
+      ? (padding = 2)
+      : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
+}
+
+function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
+  let ov = tab.get(key) || dv;
+  tab.set(key, ov + inc);
+}
+let DEBUG = true;
+
+function dbg(expression: any, message: string = ""): any {
+  if (!DEBUG) {
+    return expression;
+  }
+  if (message) {
+    console.log(message, expression);
+  } else {
+    console.log(expression);
+  }
+  return expression;
+}
+
+function answer(part, value) {
+  console.log(`Answer ${part}: ${value}`);
+}
+
+var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  .map((s) => s.trim())
+  .filter((s) => s.length > 0);
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+contents.forEach((line) => {
+  console.log(line);
+});
