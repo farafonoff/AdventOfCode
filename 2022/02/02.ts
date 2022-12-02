5480198f8a5718f97746bde31dfa350ac809cc29
Artem Farafonov <artem_farafonov@epam.com>
2022-12-02 08:35:07 +0300

    day 2

--- 2022/02/02.ts
+++ 2022/02/02.ts
@@ -52,9 +52,60 @@ var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);
+  .filter((s) => s.length > 0)
+  .map((s) => s.split(" "));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-contents.forEach((line) => {
-  console.log(line);
+let sc1 = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+let sc2 = {
+  A: {
+    X: 3,
+    Y: 6,
+    Z: 0,
+  },
+  B: {
+    X: 0,
+    Y: 3,
+    Z: 6,
+  },
+  C: {
+    X: 6,
+    Y: 0,
+    Z: 3,
+  },
+};
+let tsc = 0;
+contents.forEach((line, ln) => {
+  let rsc = sc1[line[1]] + sc2[line[0]][line[1]];
+  //console.log(ln, rsc);
+  tsc += rsc;
 });
+answer(1, tsc);
+
+tsc = 0;
+contents.forEach((line, ln) => {
+  //let rsc = sc1[line[1]] + sc2[line[0]][line[1]];
+  let o1;
+  switch (line[1]) {
+    case "X": {
+      o1 = 0;
+      break;
+    }
+    case "Y": {
+      o1 = 3;
+      break;
+    }
+    case "Z": {
+      o1 = 6;
+      break;
+    }
+  }
+  let re = Object.entries(sc2[line[0]]).filter(([k, v]) => v === o1)[0];
+  let rsc = sc1[re[0]] + sc2[line[0]][re[0]];
+  tsc += rsc;
+});
+answer(2, tsc);
