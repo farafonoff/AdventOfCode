8ff73d74db5b4f66443ebeefa0c370c4b0095778
Artem Farafonov <artem_farafonov@epam.com>
2022-12-05 08:20:15 +0300

    day 5 draft

---
+++ 2022/05/05.ts
@@ -0,0 +1,92 @@
+import * as fs from "fs";
+import HM from "hashmap";
+import md5 from "js-md5";
+import PQ from "js-priority-queue";
+import _ from "lodash";
+const infile = process.argv[2] || "input";
+
+function trnum(val: string): number | string {
+  let nn = Number(val);
+  if (val !== "" && isFinite(nn)) {
+    return nn;
+  }
+  return val;
+}
+function decimalToHex(d, padding) {
+  var hex = Number(d).toString(16);
+  padding =
+    typeof padding === "undefined" || padding === null
+      ? (padding = 2)
+      : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
+}
+
+function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
+  let ov = tab.get(key) || dv;
+  tab.set(key, ov + inc);
+}
+let DEBUG = true;
+
+function dbg(expression: any, message: string = ""): any {
+  if (!DEBUG) {
+    return expression;
+  }
+  if (message) {
+    console.log(message, expression);
+  } else {
+    console.log(expression);
+  }
+  return expression;
+}
+
+function answer(part, value) {
+  console.log(`Answer ${part}: ${value}`);
+}
+
+var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  //.map((s) => s.trim())
+  .filter((s) => s.length > 0);
+//.map((s) => s.split(" "));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+let cratez = [];
+let movz = [];
+contents.forEach((line) => {
+  if (line.charAt(0) === "m") {
+    movz.push(line.split(" ").map((t) => trnum(t)));
+  } else if (line.indexOf("[") !== -1) {
+    let ci = 0;
+    for (let i = 1; i < line.length; i += 4) {
+      if (line.charAt(i) !== " ") {
+        cratez[ci] = cratez[ci] || [];
+        cratez[ci].push(line.charAt(i));
+      }
+      ++ci;
+    }
+  }
+});
+/*
+movz.forEach((mm) => {
+  for (let mi = 0; mi < mm[1]; ++mi) {
+    let mc = cratez[mm[3] - 1].splice(0, 1);
+    cratez[mm[5] - 1] = [...mc, ...cratez[mm[5] - 1]];
+  }
+  dbg(cratez);
+});
+dbg(cratez);
+answer(1, cratez.map((cz) => cz[0]).join(""));
+*/
+movz.forEach((mm) => {
+  let mc = cratez[mm[3] - 1].splice(0, mm[1]);
+  cratez[mm[5] - 1] = [...mc, ...cratez[mm[5] - 1]];
+  dbg(cratez);
+});
+dbg(cratez);
+answer(1, cratez.map((cz) => cz[0]).join(""));
