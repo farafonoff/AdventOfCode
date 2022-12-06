6368189ba147a5cb71af4967cd5f37e1a3426b9e
Artem Farafonov <artem_farafonov@epam.com>
2022-12-06 08:07:54 +0300

    day 6

---
+++ 2022/06/06.ts
@@ -0,0 +1,82 @@
+import * as fs from "fs";
+import HM from "hashmap";
+import md5 from "js-md5";
+import PQ from "js-priority-queue";
+import _ from "lodash";
+const infile = process.argv[2] || "input";
+
+function trnum(val: string): number | string {
+  let nn = Number(val);
+  if (val !== "" && isFinite(nn)) {
+    return nn;
+  }
+  return val;
+}
+function decimalToHex(d, padding) {
+  var hex = Number(d).toString(16);
+  padding =
+    typeof padding === "undefined" || padding === null
+      ? (padding = 2)
+      : padding;
+
+  while (hex.length < padding) {
+    hex = "0" + hex;
+  }
+
+  return hex;
+}
+
+function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
+  let ov = tab.get(key) || dv;
+  tab.set(key, ov + inc);
+}
+let DEBUG = true;
+
+function dbg(expression: any, message: string = ""): any {
+  if (!DEBUG) {
+    return expression;
+  }
+  if (message) {
+    console.log(message, expression);
+  } else {
+    console.log(expression);
+  }
+  return expression;
+}
+
+function answer(part, value) {
+  console.log(`Answer ${part}: ${value}`);
+}
+
+var contents = fs
+  .readFileSync(infile, "utf8")
+  .split("\n")
+  .map((s) => s.trim())
+  .filter((s) => s.length > 0);
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
+//var contents = fs.readFileSync('input', 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
+let solve = (part) => {
+  let bs;
+  switch (part) {
+    case 1:
+      bs = 4;
+      break;
+    case 2:
+      bs = 14;
+      break;
+  }
+  contents.forEach((line) => {
+    let buf = line.split("");
+    for (let i = 0; i < buf.length - bs; ++i) {
+      let sb = buf.slice(i, i + bs);
+      let ss = new Set(sb);
+      if (ss.size === bs) {
+        answer(part, i + bs);
+        break;
+      }
+    }
+  });
+};
+
+solve(1);
+solve(2);
