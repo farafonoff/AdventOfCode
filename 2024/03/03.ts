3a343fe44569f7cef73e05a061c78e8d00cd24fa
Artem Farafonov <farafonoff@gmail.com>
2024-12-02 08:31:05 +0300

    day 2

--- 2024/03/03.ts
+++ 2024/03/03.ts
@@ -50,7 +50,7 @@ function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
   let ov = tab.get(key) || dv;
   tab.set(key, ov + inc);
 }
-let DEBUG = false;
+let DEBUG = true;
 
 function dbg(expression: any, message: string = ""): any {
   if (!DEBUG) {
@@ -68,65 +68,13 @@ function answer(part, value) {
   console.log(`Answer ${part}: ${value}`);
 }
 
-/*var contents = fs
+var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
   .map((s) => s.trim())
-  .filter((s) => s.length > 0);*/
-var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/\s+/).map(Number));
+  .filter((s) => s.length > 0);
+//var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-let safe = 0;
 contents.forEach((line) => {
-  const vector = line[1] - line[0];
-  let isSafe = true;
-  dbg(vector);
-  for (let i = 0; i < line.length - 1; i++) {
-    const v2 = line[i + 1] - line[i];
-    dbg(v2);
-    if (v2 / vector > 0) {
-      const modulo = Math.abs(v2);
-      if (!(modulo >= 1 && modulo <= 3)) {
-        isSafe = false;
-      }
-    } else {
-      isSafe = false;
-    }
-  }
-  dbg(isSafe);
-  if (isSafe) {
-    safe++;
-  }
-}
-);
-
-answer(1, safe);
-
-let safe2 = 0;
-contents.forEach((line) => {
-  for (let skip = 0; skip < line.length; skip++) {
-    const goods = line.filter((_, idx) => idx !== skip);
-    const vector = goods[1] - goods[0];
-    let isSafe = true;
-    dbg(vector);
-    for (let i = 0; i < goods.length - 1; i++) {
-      const v2 = goods[i + 1] - goods[i];
-      dbg(v2);
-      if (v2 / vector > 0) {
-        const modulo = Math.abs(v2);
-        if (!(modulo >= 1 && modulo <= 3)) {
-          isSafe = false;
-        }
-      } else {
-        isSafe = false;
-      }
-    }
-    dbg(isSafe);
-    if (isSafe) {
-      safe2++;
-      break;
-    }
-  }
-}
-);
-
-answer(2, safe2);
+  console.log(line);
+});
