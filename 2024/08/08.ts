331f80bff4c4b4d065657142739381a977b4f282
Artem Farafonov <farafonoff@gmail.com>
2024-12-07 08:19:39 +0300

    day 7

--- 2024/08/08.ts
+++ 2024/08/08.ts
@@ -50,7 +50,7 @@ function incHM(tab: HM<unknown, number>, key: unknown, inc: number, dv = 0) {
   let ov = tab.get(key) || dv;
   tab.set(key, ov + inc);
 }
-let DEBUG = false;
+let DEBUG = true;
 
 function dbg(expression: any, message: string = ""): any {
   if (!DEBUG) {
@@ -75,59 +75,6 @@ var contents = fs
   .filter((s) => s.length > 0);
 //var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-const task = [];
 contents.forEach((line) => {
-  const eq = line.split(': ');
-  const [result, numbers] = [Number(eq[0]), eq[1].split(' ').map(Number)];
-  task.push([result, numbers]);
+  console.log(line);
 });
-
-function solve(result, acc, nums, idx, equation) {
-  if (idx === nums.length) {
-    if (acc === result) {
-      dbg(equation);
-    }
-    return acc === result ? true: false;
-  }
-  if (acc > result) {
-    return false;
-  }
-  const num = nums[idx];
-  return solve(result, acc + num, nums, idx + 1, `${equation} + ${num}`) || solve(result, acc * num, nums, idx + 1, `${equation} * ${num}`);
-}
-
-function solve2(result, acc, nums, idx, equation) {
-  if (idx === nums.length) {
-    if (acc === result) {
-      dbg(equation);
-    }
-    return acc === result ? true: false;
-  }
-  if (acc > result) {
-    return false;
-  }
-  const num = nums[idx];
-  return solve2(result, acc + num, nums, idx + 1, `${equation} + ${num}`) || solve2(result, acc * num, nums, idx + 1, `${equation} * ${num}`) || solve2(result, Number(String(acc) + String(num)), nums, idx + 1, `${equation} || ${num}`);
-}
-
-let ans1 = 0;
-
-task.forEach(([result, nums]) => {
-  const solvable = solve(result, nums[0], nums, 1, `${result} = ${nums[0]}`);
-  if (solvable) {
-    ans1 += result;
-  }
-});
-
-answer(1, ans1);
-
-let ans2 = 0;
-
-task.forEach(([result, nums]) => {
-  const solvable = solve2(result, nums[0], nums, 1, `${result} = ${nums[0]}`);
-  if (solvable) {
-    ans2 += result;
-  }
-});
-
-answer(2, ans2);
