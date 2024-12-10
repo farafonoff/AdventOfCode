565fb69354d741b1fed7415b6b59efe3bbca49f7
Artem Farafonov <farafonoff@gmail.com>
2024-12-10 08:29:50 +0300

    day 10

--- 2024/11/11.ts
+++ 2024/11/11.ts
@@ -68,8 +68,6 @@ function answer(part, value) {
   console.log(`Answer ${part}: ${value}`);
 }
 
-DEBUG = false;
-
 var contents = fs
   .readFileSync(infile, "utf8")
   .split("\n")
@@ -77,63 +75,6 @@ var contents = fs
   .filter((s) => s.length > 0);
 //var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.split(/[ \t]/).map(Number));
 //var contents = fs.readFileSync(infile, 'utf8').split("\n").map(s => s.trim()).filter(s => s.length > 0).map(s => s.match(/(\d+)-(\d+) (\w): (\w+)/)); // [orig, g1, g2 ...] = content
-let map = []
-let heads = []
-contents.forEach((line, ri) => {
-  let row = line.split('');
-  map.push(row.map(Number))
-  row.forEach((cell, ci) => {
-    if (cell === '0') {
-      heads.push([ri, ci])
-    }
-  })
+contents.forEach((line) => {
+  console.log(line);
 });
-
-dbg(heads)
-
-function bfs1(head) {
-  let q = [head];
-  for(let ll = 1; ll <= 9; ++ll) {
-    q = q.flatMap(p => {
-      let [r, c] = p;
-      return dirs.map(dir => {
-        let np = dir(p);
-        return np;
-      }).filter(np => _.get(map, np, -1) === ll);
-    });
-    q = _.uniqWith(q, _.isEqual);
-  }
-  return q.length;
-}
-
-let ans1 = heads.map(bfs1).reduce((a,b) => a+b);
-
-answer(1, ans1);
-
-function bfs2(head) {
-  let q = [[head, 1]];
-  for(let ll = 1; ll <= 9; ++ll) {
-    dbg(q);
-    q = q.flatMap(p => {
-      let [pos, rat] = p;
-      return dirs.map(dir => {
-        let np = dir(pos);
-        return np;
-      }).filter(np => _.get(map, np, -1) === ll)
-      .map(np => [np, rat]);
-    });
-    let ratings = new HM<number[], number>();
-    q.forEach(([np, rat]) => {
-      let ov = ratings.get(np) || 0;
-      ratings.set(np, ov + rat);
-    });
-    q = [...ratings.entries()].map(([np, rat]) => [np, rat]);
-  }
-  dbg(q, 'final');
-  let score = q.map(([pos, rat]) => rat).reduce((a,b) => a+b);
-  dbg(score, 'score');
-  return score;
-}
-
-let ans2 = heads.map(bfs2).reduce((a,b) => a+b);
-answer(2, ans2);
