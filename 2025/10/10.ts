9ec0e2a3eb56b7b3970f5ae699e6ce52c6737659
Artem Farafonov <farafonoff@gmail.com>
2025-12-10 09:49:51 +0300

    2025.10.z3

--- 2025/10/10.ts
+++ 2025/10/10.ts
@@ -4,7 +4,6 @@ import md5 from "js-md5";
 import PQ from "js-priority-queue";
 import _ from "lodash";
 import { init } from "z3-solver";
-const math = require("mathjs");
 const infile = process.argv[2] || "input";
 
 function cached<T extends Function>(fn: T): T {
@@ -123,21 +122,55 @@ function solve1(lp, bp) {
   }
 }
 
-function solve2(jr, bp) {
-  let matrix = [];
-  for(let i=0; i<jr.length; i++) {
-    matrix.push(new Array(jr.length).fill(0));
-  }
-  console.log(bp);
-  for(let b=0; b<bp.length; b++) {
-    for(let i=0; i<bp[b].length; i++) {
-      matrix[bp[b][i]][b] = 1;
+async function solve2(jr, bp, Z3) {
+  // Use Z3 Optimize solver for integer linear programming
+  // Variables: number of times each button is pressed
+  // Constraints: sum of button presses must equal target for each counter
+  // Objective: minimize total button presses
+  
+  const { Optimize, Int } = Z3;
+  const opt = new Optimize();
+  
+  // Create integer variables for each button (number of presses)
+  const buttonVars = bp.map((_, i) => Int.const(`b${i}`));
+  
+  // Constraint: each button press count must be >= 0
+  buttonVars.forEach(bv => {
+    opt.add(bv.ge(0));
+  });
+  
+  // Constraint: for each counter, sum of button contributions must equal target
+  for (let c = 0; c < jr.length; c++) {
+    let sum: any = Int.val(0);
+    for (let b = 0; b < bp.length; b++) {
+      // Count how many times this button increments counter c
+      const count = bp[b].filter(idx => idx === c).length;
+      if (count > 0) {
+        sum = sum.add(buttonVars[b].mul(count));
+      }
     }
+    opt.add(sum.eq(jr[c]));
   }
-  console.log(jr, matrix)
-  const solution = math.lusolve(matrix, jr);
-  console.log('Solution:', solution);
-  return 0;
+  
+  // Minimize total button presses
+  let totalPresses: any = Int.val(0);
+  buttonVars.forEach(bv => {
+    totalPresses = totalPresses.add(bv);
+  });
+  opt.minimize(totalPresses);
+  
+  // Solve
+  if (await opt.check() === 'sat') {
+    const model = opt.model();
+    let result = 0;
+    for (let b = 0; b < buttonVars.length; b++) {
+      const val = model.eval(buttonVars[b]);
+      result += Number(val.toString());
+    }
+    return result;
+  }
+  
+  return -1;
 }
 
 let ans1 = 0;
@@ -150,12 +183,18 @@ lightPatterns.forEach((lp, idx) => {
 });
 answer(1, ans1);
 
-let ans2 = 0;
-lightPatterns.forEach((lp, idx) => {
-  let bp = buttonPatterns[idx];
-  let jr = joltageRatings[idx];
-  let p2 = solve2(jr, bp);
-  console.log(`Pattern ${idx + 1}: ${p2} presses`);
-  ans2 += p2;
-});
-answer(2, ans2);
+(async () => {
+  const { Context } = await init();
+  const Z3 = Context('main');
+  
+  let ans2 = 0;
+  for (let idx = 0; idx < lightPatterns.length; idx++) {
+    let lp = lightPatterns[idx];
+    let bp = buttonPatterns[idx];
+    let jr = joltageRatings[idx];
+    let p2 = await solve2(jr, bp, Z3);
+    console.log(`Pattern ${idx + 1}: ${p2} presses`);
+    ans2 += p2;
+  }
+  answer(2, ans2);
+})();
